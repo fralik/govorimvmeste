1a0d739c4506f90897adff4f59475815b9ec1a3a
Vadim Frolov <fralik@gmail.com>
2011-01-18 13:41:25 +0100

    Added Mailer

---
+++ code/app/controllers/components/mailer.php
@@ -0,0 +1,17 @@
+<?php
+
+App::import('Component', 'SwiftMailer');
+class MailerComponent extends SwiftMailerComponent
+{
+    var $smtpType = 'tls'; 
+	var $smtpHost = 'smtp.gmail.com'; 
+	var $smtpPort = 465;
+	var $smtpUsername = 'user@domain'; 
+	var $smtpPassword = 'secret_password'; 
+
+	var $sendAs = 'text'; 
+	var $from = 'mail@govorimvmeste.com'; 
+	var $fromName = 'GovorimVmeste'; 
+	var $template = 'default';
+}
+?>
