1d5fa541b5c02cc907b7287e1b94031ce7db8492
Vadim Frolov <fralik@gmail.com>
2011-01-18 13:48:44 +0100

    Added Readme, added INSTALL comment to Mailer

--- code/app/controllers/components/mailer.php
+++ code/app/controllers/components/mailer.php
@@ -6,7 +6,7 @@ class MailerComponent extends SwiftMailerComponent
     var $smtpType = 'tls'; 
 	var $smtpHost = 'smtp.gmail.com'; 
 	var $smtpPort = 465;
-	var $smtpUsername = 'user@domain'; 
+	var $smtpUsername = 'user@domain'; // INSTALL: configure for your needs
 	var $smtpPassword = 'secret_password'; 
 
 	var $sendAs = 'text'; 
